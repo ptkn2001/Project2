e5887e22e5364605aaef00ad2163ddbf7e53b3b2
Derrick Nguyen <ptkn2001@msn.com>
2021-09-24 23:42:03 -0700

    udpate seed to not required database to be created first

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,24 @@
+const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
-
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
 
-const seedDatabase = async() => {
-    await sequelize.sync({ force: true });
+const createDatabase = async() => {
+    const config = sequelize.config;
+    const sequelize2 = new Sequelize(`mysql://${config.username}:${config.password}@${config.host}:${config.port}`);
+    await sequelize2.query(`DROP DATABASE IF EXISTS ${config.database};`);
+    await sequelize2.query(`CREATE DATABASE ${config.database};`);
+}
 
+const seedDatabase = async() => {
+    await createDatabase();
+    await sequelize.sync({ force: true });
     await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
-
     await Project.bulkCreate(projectData);
-
     process.exit(0);
 };
 
