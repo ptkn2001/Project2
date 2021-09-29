d094e7ab75e6f70e6c15fae56cb989da6dd6c864
Derrick Nguyen <ptkn2001@msn.com>
2021-09-29 13:18:19 -0700

    made changes to the models to them more roburst

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -4,7 +4,8 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async(req, res) => {
     try {
-
+        //project has two many variations so we don't check for the existent of the project before adding.
+        //if somehow the user mistakenly create a new project, she/he can delete it easisy from the site.
         const projectData = await Project.create({
             name: req.body.projectName,
             description: req.body.projectDescription,
@@ -26,7 +27,7 @@ router.delete('/:id', withAuth, async(req, res) => {
         await Project.destroy({
             where: {
                 id: req.params.id,
-                //We are making sure that the person that wants to delete the project is the project's organizer.
+                //We are making sure that the person that requests to delete the project is the project's owner.
                 organizer_id: req.session.user_id,
             },
         });
