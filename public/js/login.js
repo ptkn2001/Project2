ca25ae42e71e691009398c1255ac4b4832129cc0
Ben Koala <bkoala123@hotmail.com>
2021-09-29 20:31:53 -0700

    Updates to login files

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async(event) => {
     const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
-          console.log ("Eter post")
+          
         // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', {
             method: 'POST',
