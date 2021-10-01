5a70e3230e3d00cbfa26c687ff6e226b5455afa9
Derrick Nguyen <ptkn2001@msn.com>
2021-09-30 21:20:01 -0700

    fix config issue

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 
 module.exports = sequelize;
