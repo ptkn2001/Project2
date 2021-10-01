750edc5a8f8cff0d08ea210ce755ca7d6aa25f53
Derrick Nguyen <ptkn2001@msn.com>
2021-09-30 21:12:26 -0700

    fix config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(
-    'heroku_d248daa1fbf1b66',
-    'b18dcf67053d5e',
-    '6a2c6346', {
-        host: 'us-cdbr-east-04.cleardb.com',
-        dialect: 'mysql'
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
